87c17ea9ac1f01c0c003eca0ef26ac3ec24987c5
Awujoola <danielawujoola1@gmail.com>
2018-01-25 07:04:41 +0100

    Laravel Passport for Api Authentication added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Route;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        $rule = [
+            'email'=>'required',
+            'password'=>'required'
+        ];
+        $this->validate($request,$rule);
+
+        $params = $this->requestParams('password',['username'=>$request->email,'password'=>$request->password]);
+        $request->request->add($params);
+        $proxy = Request::create('oauth/token','POST');
+        return Route::dispatch($proxy);
+    }
+
+    public function requestParams($grant_type='password',$data){
+        return [
+            'grant_type'=>$grant_type,
+            'client_id'=>'2',
+            'client_secret'=>'jGJrceniq9PHN0AuEEQiWFXY58nMIxvz1x4kVix4',
+            'username'=>$data['username'],
+            'password'=>$data['password'],
+            'scope'=>'*'
+        ];
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,6 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+
+        Passport::routes();
+
+        Passport::tokensExpireIn(now()->addHours(3)); //expires 3 hours after
+        Passport::refreshTokensExpireIn(now()->addDays(10)); //expires 10 days after
+                
     }
 }
